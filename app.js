d39d5a3cb641286dc85d110fa0bc487d9dfe01dc
mudassir-arif <mudassir@aptechgdn.net>
2024-10-11 11:04:00 +0500

    navbar issue is resolved

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ menu.style.top = "-100%";
 
 bars.addEventListener("click" , function(){
     if(menu.style.top === "-100%"){
-        menu.style.transition = "all 0.5s ease 0s"
+        menu.style.transition = "all 1s ease 0s"
         menu.style.top = "32vh";
     }else{
         menu.style.top = "-100%";
