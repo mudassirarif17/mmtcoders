64cafd386f6fea029d3df7f7bc140b080014dcde
mudassir-arif <mudassir@aptechgdn.net>
2024-10-12 13:47:55 +0500

    Tehami Committed to Check Nav on Phone

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ menu.style.top = "-100%";
 bars.addEventListener("click" , function(){
     if(menu.style.top === "-100%"){
         menu.style.transition = "all 1s ease 0s"
-        menu.style.top = "32vh";
+        menu.style.top = "30.7vh";
     }else{
         menu.style.top = "-100%";
     }
